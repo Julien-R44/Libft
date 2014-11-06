2804519920ccba08c0278ed622d61f52f67ec55b
jripoute <jripoute@student.42.fr>
2014-11-06 06:04:31 +0100

    ok

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,50 +6,50 @@
 /*   By: jripoute <jripoute@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 20:34:00 by jripoute          #+#    #+#             */
-/*   Updated: 2014/11/05 19:09:33 by jripoute         ###   ########.fr       */
+/*   Updated: 2014/11/06 06:01:33 by jripoute         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	skip_space(const char *s, int i, int *isNeg)
+static int		skip_space(const char *s, int i, int *is_neg)
 {
-	while ((s[i] == ' ' || s[i] == '-' || s[i] == '+') || (s[i] >= 9 && s[i] <= 13))
+	while ((s[i] == ' ' || s[i] == '-' || s[i] == '+') ||
+	(s[i] >= 9 && s[i] <= 13))
 	{
-		if (*isNeg)
+		if (*is_neg)
 			return (0);
 		if (s[i] == '-')
 		{
-			if (*isNeg)
+			if (*is_neg)
 				return (0);
-			*isNeg = -1;
+			*is_neg = -1;
 		}
 		else if (s[i] == '+')
 		{
-			if (*isNeg)
+			if (*is_neg)
 				return (0);
-			*isNeg = 1;
+			*is_neg = 1;
 		}
 		i++;
 	}
-	if (*isNeg == 0)
-		*isNeg = 1;
+	if (*is_neg == 0)
+		*is_neg = 1;
 	return (i);
 }
 
-
-int		ft_atoi(const char *s)
+int				ft_atoi(const char *s)
 {
-	int i;
-	int result;
-	int value;
-	int	isNeg;
+	int			i;
+	int			result;
+	int			value;
+	int			is_neg;
 
 	i = 0;
 	result = 0;
 	value = 0;
-	isNeg = 0;
-	i = skip_space(s, i, &isNeg);
+	is_neg = 0;
+	i = skip_space(s, i, &is_neg);
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
@@ -58,8 +58,8 @@ int		ft_atoi(const char *s)
 			value = (value * 10) + result;
 		}
 		else
-			return (value * isNeg);
+			return (value * is_neg);
 		i++;
 	}
-	return (value * isNeg);
+	return (value * is_neg);
 }
