159414a3a144c29f88681437cb64281825a4f96f
Julien RIPOUTEAU <jripoute@e1r11p21.42.fr>
2014-11-12 19:17:15 +0100

    x

---
+++ srabah/ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: srabah <srabah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/10 00:20:47 by srabah            #+#    #+#             */
+/*   Updated: 2014/11/11 07:01:52 by srabah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	result = (char*)malloc(sizeof(char) *
+			((ft_strlen(s1) + ft_strlen(s2) + 1)));
+	if (!result)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+		result[i++] = s2[j++];
+	result[i] = '\0';
+	return (result);
+}
